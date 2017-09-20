07ab8c535328f05d6c4847cfee2cd42dbb0dbd9b
ImrankEDW <31934203+ImrankEDW@users.noreply.github.com>
2017-09-20 01:50:01 +0000

    Update NamedTuple.py

--- peopleeda/NamedTuple.py
+++ peopleeda/NamedTuple.py
@@ -22,10 +22,21 @@ if __name__ == "__main__":
 	sc = spark.sparkContext;
 	
 	dfColumns = ['age', 'workclass', 'finalweight', 'education', 'educationnum', 'maritalstatus', 'occupation', 'relationship', 'race', 'gender', 'capitalgain', 'capitalloss', 'hoursperweek', 'country', 'salary']
+	Employee = namedtuple('Employee', dfColumns);
 
 	def eachline_to_employee(line):
 		cell = line.split(", ");
 		return Employee(int(cell[0]), cell[1], cell[2], cell[3], cell[4], cell[5], cell[6], cell[7], cell[8], cell[9], cell[10], cell[11], cell[12], cell[13], cell[14]);
 
-	rdd = sc.read(filepath);
-	emp_rdd = rdd.map(eachline_to_employee)
+	rdd = sc.textFile(filepath);
+	emp_rdd = rdd.map(eachline_to_employee);
+
+	empdf.schema;
+	empdf.describe;
+
+	empdf.show(1);
+
+	spark.stop();
+
+
+	
